4c41ccbbf3751ab773fe6db6fcb62f98262415da
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2015-10-26 00:58:06 -0300

    chat app

---
+++ chatApp.js
@@ -0,0 +1,79 @@
+Messages = new Mongo.Collection("msgs");
+Rooms = new Meteor.Collection("rooms");
+
+Meteor.methods({
+    sendMessage: function(message, room) {
+        if (!Meteor.userId()) {
+            throw new Meteor.Error("not-authorized");
+        }
+        Messages.insert({
+            username: Meteor.user().username,
+            text: message,
+            room: room,
+            createdAt: new Date()
+        });
+    }
+});
+
+if (Meteor.isServer) {
+    Meteor.startup(function() {
+        Messages.remove({});
+        Rooms.remove({});
+        if (Rooms.find().count() === 0) {
+            ["Meteor", "JavaScript", "Reactive", "MongoDB", "Polimer", "Angularjs"].forEach(function(r) {
+                Rooms.insert({roomname: r});
+            });
+        }
+    });
+
+    Meteor.publish("messages", function() {
+        return Messages.find({}, {sort: {createdAt: -1}});
+    });
+
+    Meteor.publish("rooms", function() {
+        return Rooms.find();
+    });
+}
+
+if (Meteor.isClient) {
+    Meteor.subscribe("messages");
+    Meteor.subscribe("rooms");
+    Session.setDefault("currentRoom", "Meteor");
+
+    Template.body.helpers({
+        recentMessages: function() {
+            return Messages.find({room: Session.get('currentRoom')}, {sort: {createdAt: 1}});
+        },
+        currentRoom: function() {
+            return Session.get("currentRoom");
+        },
+        timestamp: function() {
+            return this.createdAt.toLocaleString();
+        },
+        rooms: function() {
+            return Rooms.find();
+        },
+        roomstyle: function() {
+            return Session.equals("currentRoom", this.currentRoom) ? "text-success" : 'text-muted';
+        }
+    });
+
+    Template.body.events({
+        "submit .new-message": function(event) {
+            var text = event.target.text.value;
+            var currentRoom = Session.get("currentRoom");
+
+            Meteor.call("sendMessage", text, currentRoom);
+            event.target.text.value = '';
+            return false;
+        },
+        'click li': function(event) {
+            Session.set("currentRoom", event.target.innerText);
+            ServerSession.set('currentRoom', event.target.innerText);
+        }
+    });
+
+    Accounts.ui.config({
+        passwordSignupFields: "USERNAME_ONLY"
+    });
+}
